0df1340c1edeb841f66c686a028a9302fad6601e
HaiYen180101 <haiyenthptss@gmail.com>
2023-06-06 22:24:15 +0700

    tiente

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap tien: ");
+        float tien = scanner.nextFloat();
+        System.out.println("Chuyen doi tien te: " + (tien*23000) +" VND");
+
+        }
+    }
